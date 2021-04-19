11110b0eb1960e4885187694d3ddf9dd0992b8dc
Idam <i.faizin@cebes.co.id>
2021-04-19 16:09:49 +0700

    add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <NavBar></NavBar>
+  <SideNav></SideNav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import SideNav from './components/SideNav.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    NavBar,
+    SideNav,
   }
 })
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="fixed top-0 left-0 w-full bg-gray-50 border shadow-sm px-4 z-50 md:py-0 md:pr-12 pb-24">
+        <div class="flex items-center h-14 justify-between md:h-16">
+            <img src="../assets/perfil.jpg" alt="perfil" class="rounded-full w-10 h-10 md:order-1">
+
+            <a href="#" class="text-gray-500 font-medium hidden md:block">Brand-name</a>
+
+            <!-- Search -->
+            <div class="flex py-2 px-3 bg-gray-200 rounded-md w-9/12 sm:w-5/12">
+                <input type="search" placeholder="Search" class="w-full border-none outline-none bg-gray-200">
+                <i class='bx bx-search text-xl'></i>
+            </div>
+
+            <!-- Menu Toggle -->
+            <div class="text-gray-700 cursor-pointer text-xl md:hidden">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+    name: 'NavBar'
+})
+</script>
+
+<style scoped>
+
+</style>
